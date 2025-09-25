7a0dcf19f352e7e0fac70861fba115cbfc0e39b7
Milana <fenik6321@gmail.com>
2025-09-25 13:22:19 +0300

    Выполнен расчет значения f.

--- Program.cs
+++ Program.cs
@@ -10,9 +10,10 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.Write(&quot; Введите значения параметра x = &quot;);
+            Console.Write("Введите значения параметра x = ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
+            double F = Math.Sin(x) + Math.Cos(x);
         }
     }
 }
